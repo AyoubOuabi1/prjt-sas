cb6bae7ed19b7ad3a1fb4580899b559768e20cc3
Ayoub Ouabi <112890204+AyoubOuabi1@users.noreply.github.com>
2022-09-18 20:47:39 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//structs sections
+struct Produit{
+    char  code[20];
+    char  nom[20];
+    int   quantite;
+    float prix;
+};
+
+//funftions section
+void insertIntoFile(int n){
+    FILE *fptr;
+    int i;
+    struct Produit p[n];
+    fptr = fopen("ProduitData.txt","a");
+    for(i = 0; i < n; ++i)
+    {
+        fflush(stdin);
+        //code
+        printf("saisir le code : ");
+        scanf("%s",&p[i].code);
+        fputs(p[i].code, fptr);
+        fprintf(fptr, "/");
+        //nom
+        printf("saisir le nom: ");
+        scanf("%s",&p[i].nom);
+        fputs(p[i].nom, fptr);
+        fprintf(fptr, "/");
+        //quantite
+        printf("saisir la quantite: ");
+        scanf("%d",&p[i].quantite);
+		fprintf(fptr, "%d /",p[i].quantite);
+        //prix
+        printf("saisir le prix: ");
+        scanf("%f",&p[i].prix);
+        fprintf(fptr, "%.2f\n",p[i].prix);
+    }
+    fclose(fptr);
+}
+struct Produit getAllProduct(){
+	char data[200][200];
+   
+    int lineCount=0,i;
+    FILE *file;
+    file= fopen("ProduitData.txt","r");
+    //fgets(data,200,file);
+    while(!feof(file)){
+    	if(fgets(data[lineCount],200,file)!=NULL){
+    		lineCount++;
+		}
+    	//printf("line : %s", data);
+    	
+    	 
+	}
+	//printf("line : %s", data[2]);
+	fclose(file);
+	printf("%d \n ",lineCount);
+	struct  Produit prd[lineCount];
+	for(i=0;i<lineCount;i++){
+		char* item =strtok(data[i],"/");
+		strcpy(prd[i].code,item);
+		//printf("%s\n ",item);
+		item=strtok(NULL,"/");
+		strcpy(prd[i].nom,item);
+		//printf("%s\n ",item);
+		item=strtok(NULL,"/");
+		prd[i].quantite=atof(item);
+		//printf("%s\n ",item);
+		item=strtok(NULL,"/");
+		prd[i].prix= atof(item);
+		//printf("%s\n ",item);
+	}
+	for (i=0;i<lineCount;i++){
+		printf("le code : %s / le nom : %s / la quantite est : %d / le prix : %.2f \n ",prd[i].code,prd[i].nom,prd[i].quantite,prd[i].prix);
+	}
+}
+int main() {
+    getAllProduct();
+    return 0;
+}
